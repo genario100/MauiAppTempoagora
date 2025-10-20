25417c6e3c8c1b509df06bcb411a6ba6fbe1ac44
genario100 <76125969+genario100@users.noreply.github.com>
2025-10-20 12:23:30 -0300

    primeira etapa

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,58 @@
-﻿namespace MauiAppTempoagora
+﻿using MauiAppTempoagora.Models;
+
+namespace MauiAppTempoagora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
-        }
+        }// Fim do construtor
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void OnCounterClicked(object? sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            try
+            {
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-    }
-}
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    
+                Tempo? t= await Services.DataService.GetPrevisao(txt_cidade.Text.Trim());
+                    if(t != null)
+                    {
+                        string dados_previsao = $"Cidade: {txt_cidade.Text}\n" +
+                                                $"Descrição: {t.description}\n" +
+                                                $"Temperatura Mínima: {t.temp_min} °C\n" +
+                                                $"Temperatura Máxima: {t.temp_max} °C\n" +
+                                                $"Visibilidade: {t.visibility} metros\n" +
+                                                $"Nascer do Sol: {t.sunrise}\n" +
+                                                $"Pôr do Sol: {t.sunset}\n" +
+                                                $"Velocidade do Vento: {t.speed} m/s\n" +
+                                                $"Coordenadas: Lon {t.lon}, Lat {t.lat}";
+
+                        lbl_resp.Text = dados_previsao;
+                    }
+                    else
+                    {
+                      lbl_resp.Text =  "Não foi possível obter a previsão para a cidade informada.";
+                    }
+
+                }
+                else
+                {
+                    await DisplayAlert("Aviso", "Por favor, insira o nome de uma cidade.", "OK");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }// Fim do try catch
+
+
+
+        }// Fim do OnCounterClicked
+    }// Fim da classe MainPage
+}// Fim do namespace MauiAppTempoagora
---
+++ Services/DataService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MauiAppTempoagora;
+using MauiAppTempoagora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoagora.Services
+{
+    public class DataService
+    {
+        // Simulate data fetching
+        public  static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+           string chave = "5bc49166876eef6abd6b28b60d93de45";   
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}&units=metric&lang=pt";
+
+           using (HttpClient client = new HttpClient())
+           {
+               HttpResponseMessage resp = await client.GetAsync(url);
+
+               if (resp.IsSuccessStatusCode)
+               {
+                   var json = await resp.Content.ReadAsStringAsync();
+                    // Desserializar o JSON para o objeto Tempo
+                  var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)(int?)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)(int?)rascunho["sys"]["sunset"]).ToLocalTime();
+
+
+                    t = new Tempo
+                    {
+                          lon = (double?)rascunho["coord"]["lon"],
+                          lat = (double?)rascunho["coord"]["lat"],
+                          temp_min = (double?)rascunho["main"]["temp_min"],
+                          temp_max = (double?)rascunho["main"]["temp_max"],
+                          visibility = (int?)rascunho["visibility"],
+                          sunrise = sunrise.ToString(),
+                          sunset = sunset.ToString(),
+                          description = (string?)rascunho["weather"][0]["description"],
+                          main = (string?)rascunho["weather"][0]["main"],
+                          speed = (double?)rascunho["wind"]["speed"]
+                     };// Fim do objeto Tempo
+
+                }// Fim do if
+
+                return t;
+
+            }// Fim do using
+
+        }// Fim do GetPrevisao
+
+    }// Fim da classe DataService
+
+}// Fim do namespace MauiAppTempoagora.Services
